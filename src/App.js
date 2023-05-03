434143c82a984c29abe6cb3d178711de95040c2a
ahmet <ahmet55uluisik05@gmail.com>
2023-05-03 17:56:00 +0300

    created Data json and improved App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Data from "./Data.json";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Kurtlar Vadisi Konsey Üyeleri</h1>
+      <div className="inputContainer">
+        <input
+          className="search"
+          type="text"
+          placeholder="Konsey Üyesi Giriniz..."
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <div className="dataContainer">
+        {Data.filter((val) => {
+          if (searchTerm === "") {
+            return val;
+          } else if (
+            val.title
+              .toLocaleLowerCase()
+              .includes(searchTerm.toLocaleLowerCase())
+          ) {
+            return val;
+          }
+        }).map((val) => {
+          return (
+            <div className="data" key={val.id}>
+              <img src={val.image} alt="" />
+              <h3>{val.title}</h3>
+              <p>{val.Description}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
